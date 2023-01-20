8bd129f5b4d65436d4f80021321b907844067cb9
Quang Nguyen <quang13012005@gmail.com>
2023-01-21 00:33:37 +0700

    update

---
+++ hello.c
@@ -0,0 +1,8 @@
+
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void) { //Main function => side effects inside/return value
+    string answer = get_string("What's your name: ");//Tell it wwhat you mean -> answer is a string
+    printf("hello %s\n", answer); // argument 
+}
